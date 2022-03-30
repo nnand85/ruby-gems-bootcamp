96ff935f0dc6fdd5418e0036f556b2c8ddbcde5c
Naveen Nand <nnand85@gmail.com>
2022-03-30 22:30:53 +0000

    Add styling, course and user controller

--- Gemfile
+++ Gemfile
@@ -23,7 +23,8 @@ gem 'jbuilder', '~> 2.7'
 # gem 'bcrypt', '~> 3.1.7'
 gem "haml-rails", "~> 2.0"
 gem 'simple_form'
-
+gem 'faker'
+gem 'devise'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   config.hosts << "2ac4dcef67b14c68a9f12dcd089a355d.vfs.cloud9.us-west-1.amazonaws.com"
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
