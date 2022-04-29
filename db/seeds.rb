ab63f88746f01859e9a70075cfd55a0420c0a519
Naveen Nand <nnand85@gmail.com>
2022-04-29 03:58:41 +0000

    Add fields to courses and course forms

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
-User.create!(email: 'nnand85@gmail.com', password: 'Password1', password_confirmation: 'Password1')
+#User.create!(email: 'nnand85@gmail.com', password: 'Password1', password_confirmation: 'Password1')
 
 30.times do
     Course.create!([{
         title: Faker::Educator.course_name,
         description: Faker::TvShows::GameOfThrones.quotes,
-        user_id: User.last.id
+        user_id: User.last.id,
+        short_description: Faker::Quote.famous_last_words,
+        language: 'English',
+        level: 'Intermediate',
+        price: Faker::Number.between(from: 10, to: 1000)
     }])
 end
